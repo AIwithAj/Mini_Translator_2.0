131a946c6fef7cf7ef045ba9db2f8e54c670f8f0
Ajay <dancerworld60@gmail.com>
2024-06-01 16:59:35 +0530

    data validation pipeline created

--- src/Mini_Translator_T/entity/__init__.py
+++ src/Mini_Translator_T/entity/__init__.py
@@ -11,3 +11,11 @@ class DataIngestionConfig:
     valid:Path
     test:Path
     
+
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
---
+++ src/Mini_Translator_T/pipeline/stage_02_data_validation.py
@@ -0,0 +1,32 @@
+from Mini_Translator_T.components.data_validation import DataValiadtion
+from Mini_Translator_T.config.configuration import ConfigurationManager
+from Mini_Translator_T.logging import logger
+
+
+STAGE_NAME="Data Validation"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            data_validation_config = config.get_data_validation_config()
+            data_validation = DataValiadtion(config=data_validation_config)
+            data_validation.validate_all_files_exist()
+        except Exception as e:
+            raise e
+        
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
